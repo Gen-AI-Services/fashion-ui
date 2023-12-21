c186d60fd85bbd9e142e7caa0315b4fb6113e0b0
Burak Turgut <turgutburak0144@gmail.com>
2023-12-21 12:01:03 +0300

    commit

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
 import streamlit as st
 import requests
 
+st.set_page_config(page_title="Fashion", layout="wide")
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """
 
-def main():
-    st.set_page_config(page_title="My streamlit app")
-           
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+def creds_entered():
+    
+    if st.session_state["user"].strip() == "admin" and st.session_state["password"].strip() == "genaifashion":
+        st.session_state["authenticated"] = True
+    else:
+        st.session_state["authenticated"] = False
+        if not st.session_state["password"]:
+            st.warning("Please enter password.")
+        elif not st.session_state["user"]:
+            st.warning("Please enter username.")
+        else:
+            st.error("Invalid username and password :face_with_raised_eyebrow:")
+        
+def authenticate_user():
+    if "authenticated" not in st.session_state:
+        username = st.text_input(label="Username:", value="", key="user")
+        password = st.text_input(label="Password:", value="", key="password", type="password")
+        submit = st.button("Submit", on_click=creds_entered)
+
+        if submit and username == "admin" and password == "password":
+            st.session_state.authenticated = True
+
+        return False
+    else:
+        if st.session_state.authenticated:
+            return True
+        else:
+            username = st.text_input(label="Username:", value="", key="user")
+            password = st.text_input(label="Password:", value="", key="password", type="password")
+            submit = st.button("Submit", on_click=creds_entered)
+
+            if submit and username == "admin" and password == "password":
+                st.session_state.authenticated = True
+
+            return False
+
+
+if authenticate_user():
     with st.sidebar:
         uploaded_file = st.file_uploader("Bir resim yükleyin", type=["jpg", "png"])
         if uploaded_file is not None:
@@ -13,7 +58,7 @@ def main():
 
 
     user_question = st.chat_input("Enter your message")
-       
+    
     if "messages" not in st.session_state:
         st.session_state.messages = []   
     
@@ -25,23 +70,23 @@ def main():
         if uploaded_file is None:
             st.error("Bir resim yükleyin.")
     
-            
     if uploaded_file is not None:
         if user_question:
             st.session_state.messages.append({"role": "user", "content": user_question})
             with st.chat_message("user"):
                 st.write(user_question)
-                
-            url = "http://4.255.89.202/api/v1/vision" 
+            spinner = st.spinner("Thinking...") 
+            url = "https://fashion-backend-18a4640a2fd7.herokuapp.com/api/v1/uid/gpt4v" 
             files = {'image': uploaded_file.getvalue()}
-            response = requests.post(url, files=files, params={"question": user_question})
-            response = (response.json()['gpt']["choices"][0]["message"]["content"])
+            try:
+                with spinner:
+                    response = requests.post(url, files=files, params={"question": user_question})
+                    response = (response.json()['gpt']["choices"][0]["message"]["content"])
+                    st.session_state.messages.append({"role": "assistant", "content": response})
+                with st.chat_message("assistant"):
+                    st.write(response)
             
-            st.session_state.messages.append({"role": "assistant", "content": response})
-            
-            with st.chat_message("assistant"):
-                st.write(response)
-            
+            except Exception as e:
+                st.error("An error occurred. Please try again later.")
 
-if __name__ == '__main__':
-    main()
+
